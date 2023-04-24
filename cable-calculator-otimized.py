0ccd262d2534a54643e6409a01e3d272038552e9
Wellington Jackson <wellingtonjsramos@hotmail.com>
2023-04-24 20:30:44 -0300

    Implemented to get from xlsx archive

--- cable-calculator-otimized.py
+++ cable-calculator-otimized.py
@@ -1,13 +1,14 @@
 from itertools import combinations
 import pprint
+import pandas as pd
 
 
 class InstallationOtimized:
     def __init__(self):
         # Format: {INT Length: INT Qnt}
-        self.cameras = {150: 2, 130: 1, 110: 3, 90: 2, 60: 3}
+        self.cameras = {}
         # Format: INT length
-        self.rolls_size = 300
+        self.rolls_size = 305
         # Format: INT rolls
         self.rolls_used = 0
         # Format: [INT length]
@@ -15,63 +16,30 @@ class InstallationOtimized:
         # Format: {STR Name_Roll: {STR Name_Cam: INT length, STR Scraps: INT length}} # noqa E501
         self.organization = {}
 
-    # ADICIONAR CAMERAS AO OBJETO
-    def add_cameras(self):
-        # checagem para metragem das cameras a serem adicionadas
-        print("Qual a metragem das cameras que você irá adicionar?")
-        camera_input_length = int(input().strip())
-        # checagem para quantidade com base na metragem
-        print(f"Quantas cameras de {camera_input_length} você irá adicionar?")
-        camera_input_qnt = int(input().strip())
-        # verificar se a metragem já existe
-        if camera_input_length in self.cameras:
-            # soma se já existir
-            self.cameras[camera_input_length] += camera_input_qnt
-        else:
-            # cria e atribui o valor
-            self.cameras[camera_input_length] = camera_input_qnt
-        # retorno com a lista atualizada
-        print(self.cameras)
-
-    # DELETAR CAMERAS DO OBJETO
-    def del_cameras(self):
-        # checagem para metragem das cameras a serem adicionadas
-        print("Qual a metragem das cameras que você procura?")
-        camera_input_length = int(input().strip())
-        # verificar se a metragem já existe
-        if camera_input_length in self.cameras:
-            # se existir, prossegue para as quantidades
-            print(f'Existem {self.cameras[camera_input_length]} adicionadas, quantas deseja remover?')  # noqa E501
-            camera_input_qnt = int(input().strip())
-            # checa se será apenas uma subtração ou se irá remover todas
-            if self.cameras[camera_input_length] > camera_input_qnt:
-                # subtrai se o numero for menor do que existe
-                self.cameras[camera_input_length] -= camera_input_qnt
+    # BAIXA CAMERAS E TOTAIS DA PLANILHA
+    def table_cameras(self, tb_name):
+        # colunas antes do cabeçalho
+        pre_header = 3
+        # lista completa da planilha
+        table = pd.read_excel(tb_name, 'POSICIONAMENTO',
+                            header=None, skiprows=pre_header).values.tolist()
+        # titulo do cabeçalho sendo procurado
+        search_cam = 'CÂMERA'
+        search_length = 'TOTAL'
+        # index da coluna
+        index_cam = table[0].index(search_cam)
+        index_length = table[0].index(search_length)
+        # listando todos os nomes de câmeras
+        camera_names = {}
+        for i, cam in enumerate(table[1:-1]):
+            if cam[index_cam].startwith('CF'):
+                camera_names[f'CF{int(cam[index_cam].replace("CF", "")).zfill(3)}'] = int(cam[index_length])
+            elif cam[index_cam].startwith('CD'):
+                camera_names[f'CD{int(cam[index_cam].replace("CD", "")).zfill(3)}'] = int(cam[index_length])
             else:
-                # apaga a chave se for maior
-                del self.cameras[camera_input_length]
-        else:
-            # informa que não foram localizadas cameras com essa metragem
-            print(
-                f'Não existem cameras com {camera_input_length} adicionadas.'
-            )
-        # retorno com a lista atualizada
-        print(self.cameras)
-
-    # DEFINE O TAMANHO PADRÃO DOS ROLOS DO OBJETO
-    def set_roll(self):
-        # questiona novo tamanho informando o tamanho atual
-        print(f'Qual o novo tamanho do rolo? (atual: {self.rolls_size})')
-        roll = int(input().strip())
-        # checa se o valor informado é o mesmo
-        if roll == self.rolls_size:
-            # informa que o tamanho foi o mesmo
-            print('O tamanho inserido foi o mesmo, portanto o valor mantido.')
-        else:
-            # altera o valor
-            self.rolls_size = roll
-        # retorno o tamanho do rolo
-        print(self.rolls_size)
+                camera_names[f'ID{str(i+1).zfill(3)} {cam[index_cam]}'] = int(cam[index_length])
+        self.cameras = camera_names
+        print(f'{len(camera_names)} listados.')
 
     # RETORNA A LISTA COM TODOS OS DADOS DO OBJETO
     def get_list(self):
@@ -82,20 +50,6 @@ class InstallationOtimized:
             'scraps': self.scraps,
             'organization': self.organization,
         }
-
-    # FUNÇÃO CHAMADA POR install_organizer(self) -> ORGANIZA AS CAMERAS POR NOME # noqa E051
-    def cam_list(self):
-        # cria a lista e numeração das cameras
-        cam_list = {}
-        cam_num = 1
-        # itera sobre os itens no dicionario das cameras
-        for length, num in self.cameras.items():
-            # com base na quantidade de cameras para a distancia monta a lista
-            for _ in range(num):
-                cam_list[f'Camera {cam_num}'] = length
-                cam_num += 1
-        # retorna a lista numerada das cameras
-        return cam_list
 
     # FUNÇÃO CHAMADA POR install_organizer(self) -> DEFINE O MELHOR GRUPO DE CAMERAS POR ROLO # noqa E051
     def define_cam(self, cam_list, rolls_size):
@@ -116,29 +70,41 @@ class InstallationOtimized:
                     # se o valor for igual ao tamanho do rolo retorna imediatamente # noqa E051
                     if value == rolls_size:
                         return best_combination
+                    #if i > 5:
+                    #    return best_combination
         # retorna a melhor combinação
         return best_combination
 
     # DEFINE A ORGANIZAÇÃO GERAL DE INSTALAÇÃO
     def install_organizer(self):
-        # organiza a lista de cameras nomeando elas
-        cam_list = self.cam_list()
+        # recupera a lista de cameras
+        cam_list = self.cameras.copy()
         organizer = {}
         roll = 0
         # realiza a função enquanto houver itens dentro da lista de cameras
         while cam_list:
             # numera o rolo
             roll += 1
-            # acha a melhor combinação de cameras para menor sobra o possível
-            defined = self.define_cam(cam_list, self.rolls_size)
-            # atribui a melhor combinação a lista organizada
-            organizer[f'Rolo nº {roll}'] = defined
-            # deleta todos os itens da lista de cameras disponíveis
-            for cam in defined.keys():
-                del cam_list[cam]
-            # adiciona as sobras
-            organizer[f'Rolo nº {roll}']['Sobra'] = self.rolls_size - \
-                sum(defined.values())
+            # se o cumprimento da camera for excedente ao rolo
+            if list(cam_list.values())[0] > self.rolls_size:
+                organizer[f'Rolo nº {str(roll).zfill(3)} Excedente'] = {
+                    list(cam_list.keys())[0]: int(list(cam_list.values())[0])
+                }
+                # adiciona os excessos
+                organizer[f'Rolo nº {str(roll).zfill(3)} Excedente']['Sobra'] = f'Excedente {self.rolls_size - int(list(cam_list.values())[0])}'
+                # elimina item da lista
+                del cam_list[list(cam_list.keys())[0]]
+            else:
+                # acha a melhor combinação de cameras para menor sobra o possível
+                defined = self.define_cam(cam_list, self.rolls_size)
+                # atribui a melhor combinação a lista organizada
+                organizer[f'Rolo nº {str(roll).zfill(3)}'] = defined
+                # deleta todos os itens da lista de cameras disponíveis
+                for cam in defined.keys():
+                    del cam_list[cam]
+                # adiciona as sobras
+                organizer[f'Rolo nº {str(roll).zfill(3)}']['Sobra'] = self.rolls_size - \
+                    sum(defined.values())
         # define a organização do objeto, rolos usados e sobras
         self.organization = organizer
         self.rolls_used = roll
@@ -146,7 +112,15 @@ class InstallationOtimized:
         # retorna a lista organizada dos rolos com cada camera e sobras
         return organizer
 
+tb_name = 'Teste.xlsx'
+#tb_name = 'DIST. DE CÂMERAS - TABOÃO DA SERRA CENTRO-CFTV.xlsx'
+#tb_name = 'DIST. DE CÂMERAS - ANHANGUERA-CFTV - HD - BRIFIENG.xlsx'
+#tb_name = 'DISTRIBUIÇÃO DE CÂMERAS - BOA VIAGEM.xlsx'
+#tb_name = 'DISTRIBUIÇÃO DE CÂMERAS - IPATINGA NOVA (1) (1).xlsx'
+#tb_name = 'DISTRIBUIÇÃO DE CÂMERAS - MARGINAL TIETE ETNA (1).xlsx'
 
 user = InstallationOtimized()
+user.table_cameras(tb_name)
+pprint.pprint(user.get_list()['cameras'])
 user.install_organizer()
 pprint.pprint(user.get_list())
